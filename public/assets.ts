d5b61948c98de68a263dfb84a6e1d36386f6d2fa
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-03-14 17:14:37 +0530

    feat: Introduce initial BUNKR survival log frontend with new HTML, CSS, and supporting assets.

--- public/assets.ts
+++ public/assets.ts
@@ -1,5 +1,6 @@
 const assets = {
-    faaaaaaa: '/audio/WhatsApp Audio 2026-03-14 at 11.42.15.mpeg'
+    faaaaaaa: '/audio/WhatsApp Audio 2026-03-14 at 11.42.15.mpeg',
+    never_gonna_give_u_up: '/gifs/WhatsApp Video 2026-03-14 at 1.01.46 PM.mp4'
 }
 
 export default assets
